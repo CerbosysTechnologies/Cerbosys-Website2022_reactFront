317e511d62505fa43501eb4183704e95ba9c2943
CerbosysTechnologies <93573082+CerbosysTechnologies@users.noreply.github.com>
2022-07-30 18:21:55 +0530

    MobileForm Changes

--- src/components/InnerPages/Services/Mobile/MobileForm.jsx
+++ src/components/InnerPages/Services/Mobile/MobileForm.jsx
@@ -20,11 +20,13 @@ const MobileForm = () => {
             {/* Form Design */}
             <div className="grid md:grid-cols-2  mb-4 md:px-36 py-5  mt-10">
               {/* Image Section */}
-              <img src={mobileservice_technologyicon} alt=""></img>
+              <div className="">
+              <img src={mobileservice_technologyicon} alt="" className="h-full"></img>
+              </div>
               {/* Image Section Ends*/}
 
               {/* Form Section */}
-              <div className="bg-white md:px-14 px-10 items-center justify-center text-center">
+              <div className="bg-white md:px-14 px-10 items-center justify-center text-center ">
                 <form>
                   {/* Name Div */}
                   <div class="relative z-0 mb-6 mt-5 w-full group">
@@ -51,7 +53,7 @@ const MobileForm = () => {
                   {/* Name Div Ends */}
 
                   {/* Email Div */}
-                  <div class="relative z-0 mb-6 w-full group">
+                  <div class="sticky z-0 mb-6 w-full group">
                     <input
                       type="email"
                       name="floating_email"
@@ -107,7 +109,8 @@ const MobileForm = () => {
                     />
                     <label
                       for="floating_cn"
-                      class="peer-focus:font-subheading absolute text-sm md:text-lg text-gray-500 dark:text-gray-400 
+                      class="peer-focus:font-subheading absolute text-sm md:text-lg
+                       text-gray-500 dark:text-gray-400 
                       duration-300 transform -translate-y-6 scale-75 top-3 left-1
                       -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600
                        peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 
@@ -119,41 +122,80 @@ const MobileForm = () => {
                   {/* Subject Div Ends */}
 
                   {/* Selection Div */}
-                  <div className="w-full">
-                    <button
-                      id="dropdownDividerButton"
-                      data-dropdown-toggle="dropdownDivider"
-                      class="text-gray-400 font-subheading md:text-lg text-sm flex
-                      border-2 rounded-xl px-2 py-2 md:w-[250px]"
-                      type="button"
-                    >
-                      Service
-                      <svg
-                        class="ml-20 mt-2 w-full h-4"
-                        aria-hidden="true"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M19 9l-7 7-7-7"
-                        ></path>
-                      </svg>
-                    </button>
+                  <div className="relative z-0 mb-3 w-full group">
+                  <select
+                    className="dropdown text-gray-400 font-subheading md:text-lg text-sm flex
+                    border-2 rounded-xl px-2 py-2 border-gray-400
+                    
+                    ">
+                    <option value="">Select</option>
+                    <option value="">Web Development</option>
+                    <option value="">Digital Marketing</option>
+                    <option value="">Lead Generation</option>
+                    <option value="">Branding and Packaging</option>
+                    <option value="">Mobile Development</option>
+                    <option value="">UI/UX Development</option>
+                    <option value="">Staff Augmentation</option>                 
+                    
+                  </select>
                   </div>
                   {/* Selection Div Ends*/}
 
-                  <textarea
-                    className=" w-full mt-5 border-solid border-2 border-gray-300  
-                    py-2 px-3 text-gray-700 leading-tight font-subheading"
-                    id="message"
-                    type="text"
-                    placeholder="Message"
-                  />
+                  {/*   Hear About Us */}
+                  <div className="group">
+                    <label className="flex font-subheading md:text-lg text-md justify-start text-gray-400">How Did You Hear About Us?</label>
+                    <div className="flex items-center mb-4">
+                      <input checked id="checkbox-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                      <label for="checkbox-Google" class="ml-2 text-sm font-subheading text-gray-400 dark:text-gray-300">Google</label>
+
+
+                      <input checked id="checkbox-1" type="checkbox" value="" class="ml-4 w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                      <label for="checkbox-LinkdIn" class="ml-2 text-sm font-subheading text-gray-400 dark:text-gray-300">LinkedIn</label>
+                    
+
+                    <input checked id="checkbox-1" type="checkbox" value="" class="ml-4 w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label for="checkbox-Facebook" class="ml-2 text-sm font-subheading text-gray-400 dark:text-gray-300">Facebook</label>
+
+                    <input checked id="checkbox-1" type="checkbox" value="" class="ml-4 w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label for="checkbox-Instagram" class="ml-2 text-sm font-subheading text-gray-400 dark:text-gray-300">Instagram</label>
+
+                 </div>
+
+                 <div className="flex items-center mb-4">
+                 <input checked id="checkbox-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label for="checkbox-FriendSuggested" class="ml-2 text-sm font-subheading text-gray-400 dark:text-gray-300">Friend Suggested</label>
+
+                    <input checked id="checkbox-1" type="checkbox" value="" class="ml-4 w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label for="checkbox-Others" class="ml-2 text-sm font-subheading text-gray-400 dark:text-gray-300">Others</label>   
+                 </div>
+                  </div>
+                  {/*   Hear About Us Ends*/}
+
+                   {/* Message Div */}
+                   <div class="relative z-0 mb-6 w-full group">
+                    <input
+                      type="tel"
+                      name="floating_cn"
+                      id="floating_cn"
+                      class="block py-2.5 px-0 w-full text-sm md:text-lg text-gray-900 
+                      bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white
+                       dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" "
+                      required
+                    />
+                    <label
+                      for="floating_cn"
+                      class="peer-focus:font-subheading absolute text-sm md:text-lg
+                       text-gray-500 dark:text-gray-400 
+                      duration-300 transform -translate-y-6 scale-75 top-3 left-1
+                      -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600
+                       peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 
+                       peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 font-subheading"
+                    >
+                      Message
+                    </label>
+                  </div>
+                  {/* Message Div Ends */}
 
                   <input
                     className="mt-6 mb-6 bg-Primary text-white font-heading py-2 px-4 rounded"
