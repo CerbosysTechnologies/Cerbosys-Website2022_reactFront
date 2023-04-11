770f55afec1c35f397c012eaee74039108453d09
CerbosysTechnologies <93573082+CerbosysTechnologies@users.noreply.github.com>
2023-04-11 18:53:58 +0530

    chnages

--- src/components/HomePage/ServiceHomePage.jsx
+++ src/components/HomePage/ServiceHomePage.jsx
@@ -12,6 +12,7 @@ import Slide from 'react-reveal/Slide';
 import Bounce from 'react-reveal/Bounce';
 import Flip from 'react-reveal/Flip';
 import { handelRightClick } from '../../components/Common/utils/Desbalrightclick';
+import { Link } from 'react-router-dom';
 
 const ServiceHomePage = () => {
   // document.removeEventListener("contextmenu", handelRightClick);
@@ -45,161 +46,168 @@ const ServiceHomePage = () => {
             items-center justify-center text-center gap-8 p-3"
             >
               {/* Mobile App Developemnt */}
+              <Link to="./mobile-app-development">
+                <div className="hover:bg-green-400 hover:cursor-pointer rounded-lg p-10 hover:text-white">
+                  <Bounce left duration={3000}>
+                    <img
+                      alt="Mobile Icon"
+                      src={mobileicon}
+                      //   scale-100 hover:scale-75 ease-out duration-500
+                      //   hover:rounded-full hover:duration-700
+                      className="w-[70px] h-[70px] mb-2  scale-100 hover:scale-75 ease-out duration-500 "
+                    />
+                  </Bounce>
 
-              <div>
-                <Bounce left duration={3000}>
-                  {' '}
-                  <img
-                    alt="Mobile Icon"
-                    src={mobileicon}
-                    //   scale-100 hover:scale-75 ease-out duration-500
-                    //   hover:rounded-full hover:duration-700
-                    className="w-[70px] h-[70px] mb-2  scale-100 hover:scale-75 ease-out duration-500 "
-                  />
-                </Bounce>
+                  <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
+                    Mobile App Developements
+                  </h2>
+                  <p className="font-content text-justify text-sm md:mb-0 mb-2 mt-4">
+                    Smartphones are a crucial part of our lives, so it's
+                    important to have an app that'll make your life easier. If
+                    you need help with app development, we have the experience
+                    and knowledge needed to create something functional and
+                    appealing for any industry and budget.
+                    <br></br>
+                    <br></br>
+                  </p>
+                </div>
+              </Link>
+              <Link to="./web-development">
+                <div className="hover:bg-green-400 hover:cursor-pointer rounded-lg p-10 hover:text-white">
+                  <Bounce top duration={3000}>
+                    {' '}
+                    <img
+                      alt="Web Icon"
+                      src={webdevelopmenticon}
+                      className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
+                    />
+                  </Bounce>
+                  <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
+                    Web Developement
+                  </h2>
+                  <p className="font-content text-justify text-sm mt-4">
+                    Website development is the process of creating a new website
+                    or updating it to a newer version. Whether you're looking
+                    for a new website or want to improve the current one, we'll
+                    make it happen. We are experts in designing and developing
+                    websites that are appealing to both users and search engine
+                    crawlers alike.
+                  </p>
+                </div>
+              </Link>
 
-                <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
-                  Mobile App Developements
-                </h2>
-                <p className="font-content text-justify text-sm md:mb-0 mb-2 mt-4">
-                  Smartphones are a crucial part of our lives, so it's important
-                  to have an app that'll make your life easier. If you need help
-                  with app development, we have the experience and knowledge
-                  needed to create something functional and appealing for any
-                  industry and budget.
-                  <br></br>
-                  <br></br>
-                </p>
-              </div>
+              <Link to="./digital-marketing">
+                <div className="hover:bg-green-400 hover:cursor-pointer rounded-lg p-10 hover:text-white">
+                  <Bounce right duration={3000}>
+                    {' '}
+                    <img
+                      alt="Digital Marketing Icon"
+                      src={digitalmarketingicon}
+                      className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
+                    />
+                  </Bounce>
+                  <h2 className="font-subheading text-left text-xl w-1/3 mt-4">
+                    Digital Marketing
+                  </h2>
+                  <p className="font-content text-justify text-sm mt-4">
+                    As a software development and marketing agency, we know what
+                    it takes to get real results online. These include building
+                    an effective digital marketing strategy and executing a
+                    purposeful, data-driven campaign. From start to finish, our
+                    team will run your campaigns from A-Z and get you real
+                    RESULTS!
+                  </p>
+                </div>
+              </Link>
 
-              <div>
-                <Bounce top duration={3000}>
-                  {' '}
-                  <img
-                    alt="Web Icon"
-                    src={webdevelopmenticon}
-                    className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
-                  />
-                </Bounce>
-                <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
-                  Web Developement
-                </h2>
-                <p className="font-content text-justify text-sm mt-4">
-                  Website development is the process of creating a new website
-                  or updating it to a newer version. Whether you're looking for
-                  a new website or want to improve the current one, we'll make
-                  it happen. We are experts in designing and developing websites
-                  that are appealing to both users and search engine crawlers
-                  alike.
-                </p>
-              </div>
-
-              <div>
-                <Bounce right duration={3000}>
-                  {' '}
-                  <img
-                    alt="Digital Marketing Icon"
-                    src={digitalmarketingicon}
-                    className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
-                  />
-                </Bounce>
-                <h2 className="font-subheading text-left text-xl w-1/3 mt-4">
-                  Digital Marketing
-                </h2>
-                <p className="font-content text-justify text-sm mt-4">
-                  As a software development and marketing agency, we know what
-                  it takes to get real results online. These include building an
-                  effective digital marketing strategy and executing a
-                  purposeful, data-driven campaign. From start to finish, our
-                  team will run your campaigns from A-Z and get you real
-                  RESULTS!
-                </p>
-              </div>
-
-              <div>
-                <Bounce left duration={3000}>
-                  {' '}
-                  <img
-                    alt="Staff Augmentation Icon"
-                    src={staffaugmentationicon}
-                    className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
-                  />
-                </Bounce>
-                <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
-                  Staff Augmentation
-                </h2>
-                <p className="font-content text-justify text-sm md:mb-0 mb-16 mt-4">
-                  Since one of the primary elements of staff augmentation is
-                  talent, Cerbosys acts as a link to augment the capacity of
-                  your organization and we can help your company to build an
-                  entirely effective team that can add good value to your brand
-                  and projects.
-                </p>
-              </div>
-
-              <div>
-                <Bounce bottom duration={3000}>
-                  {' '}
-                  <img
-                    alt="UI/UX Icon"
-                    src={uxdevelopmenticon}
-                    className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
-                  />
-                </Bounce>
-                <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
-                  UI/UX Development
-                </h2>
-                <p className="font-content text-justify text-sm md:mb-0 mb-12 mt-4">
-                  We have a team that is especially concerned about the entire
-                  process of product design. Apart from designing and
-                  useability, we also provide a meaningful and enjoyable
-                  experience for the users. Since a better user experience can
-                  increase your sales effectively.
-                </p>
-              </div>
-
-              <div>
-                <Bounce right duration={3000}>
-                  {' '}
-                  <img
-                    alt="Branding And Packaging Icon"
-                    src={brandpackingicon}
-                    className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
-                  />
-                </Bounce>
-                <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
-                  Branding And Packaging
-                </h2>
-                <p className="font-content text-justify text-sm mt-4">
-                  We serve your product from branding to packaging, that is,
-                  creating a unique identity of your product by designing the
-                  brand logo graphics and designing its packaging to make it
-                  stand different from other products. We effectively serve our
-                  clients and help their brand to draw customer attention.
-                </p>
-              </div>
-
-              <div>
-                <Bounce left duration={3000}>
-                  {' '}
-                  <img
-                    alt="Lead Generation Icon"
-                    src={leadrgenerationicon}
-                    className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
-                  />
-                </Bounce>
-                <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
-                  Lead Generation
-                </h2>
-                <p className="font-content text-justify text-sm mb-16 mt-4">
-                  Getting high-quality leads and clients through Search Engine
-                  Optimization (SEO) and Social Media Marketing (SMM) - Drive
-                  higher engagement rates for your brand on social media
-                  platforms by having a social media marketing campaign tailored
-                  to your needs.
-                </p>
-              </div>
-
+              <Link to="./staff-augmentation">
+                <div className="hover:bg-green-400 hover:cursor-pointer rounded-lg p-10 hover:text-white">
+                  <Bounce left duration={3000}>
+                    {' '}
+                    <img
+                      alt="Staff Augmentation Icon"
+                      src={staffaugmentationicon}
+                      className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
+                    />
+                  </Bounce>
+                  <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
+                    Staff Augmentation
+                  </h2>
+                  <p className="font-content text-justify text-sm md:mb-0 mb-16 mt-4">
+                    Since one of the primary elements of staff augmentation is
+                    talent, Cerbosys acts as a link to augment the capacity of
+                    your organization and we can help your company to build an
+                    entirely effective team that can add good value to your
+                    brand and projects.
+                  </p>
+                </div>
+              </Link>
+              <Link to="./ui-ux-designing">
+                <div className="hover:bg-green-400 hover:cursor-pointer rounded-lg p-10 hover:text-white">
+                  <Bounce bottom duration={3000}>
+                    {' '}
+                    <img
+                      alt="UI/UX Icon"
+                      src={uxdevelopmenticon}
+                      className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
+                    />
+                  </Bounce>
+                  <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
+                    UI/UX Development
+                  </h2>
+                  <p className="font-content text-justify text-sm md:mb-0 mb-12 mt-4">
+                    We have a team that is especially concerned about the entire
+                    process of product design. Apart from designing and
+                    useability, we also provide a meaningful and enjoyable
+                    experience for the users. Since a better user experience can
+                    increase your sales effectively.
+                  </p>
+                </div>
+              </Link>
+              <Link to="./branding-and-packaging">
+                <div className="hover:bg-green-400 hover:cursor-pointer rounded-lg p-10 hover:text-white">
+                  <Bounce right duration={3000}>
+                    {' '}
+                    <img
+                      alt="Branding And Packaging Icon"
+                      src={brandpackingicon}
+                      className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
+                    />
+                  </Bounce>
+                  <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
+                    Branding And Packaging
+                  </h2>
+                  <p className="font-content text-justify text-sm mt-4">
+                    We serve your product from branding to packaging, that is,
+                    creating a unique identity of your product by designing the
+                    brand logo graphics and designing its packaging to make it
+                    stand different from other products. We effectively serve
+                    our clients and help their brand to draw customer attention.
+                  </p>
+                </div>
+              </Link>
+              <Link to="./lead-generation">
+                <div className="hover:bg-green-400 hover:cursor-pointer rounded-lg p-10 hover:text-white">
+                  <Bounce left duration={3000}>
+                    {' '}
+                    <img
+                      alt="Lead Generation Icon"
+                      src={leadrgenerationicon}
+                      className="w-[70px] h-[70px] mb-2 scale-100 hover:scale-75 ease-out duration-500"
+                    />
+                  </Bounce>
+                  <h2 className="font-subheading text-left text-xl w-1/2 mt-4">
+                    Lead Generation
+                  </h2>
+                  <p className="font-content text-justify text-sm mb-16 mt-4">
+                    Getting high-quality leads and clients through Search Engine
+                    Optimization (SEO) and Social Media Marketing (SMM) - Drive
+                    higher engagement rates for your brand on social media
+                    platforms by having a social media marketing campaign
+                    tailored to your needs.
+                  </p>
+                </div>
+              </Link>
               <div className="md:col-span-2 col-span-1">
                 <img
                   src={quoteicon}
