8f6ae230f2623b3c39ec2e8a24f5db058b878050
CerbosysTechnologies <team.android@cerbosys.com>
2023-07-04 14:53:41 +0530

    4-7-2023

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+import React, { useEffect } from "react";
+
+import FreeQuote from "./components/Common/Component/FreeQuote";
+
+import { ScrollToTop } from "../src/components/Common/Component/ScrollToTop";
+
+import { handelRightClick } from "../src/components/Common/utils/Desbalrightclick";
+
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Common/Component/Footer";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Routers from "./Routers.js";
+import { useScroll, useSpring } from "framer-motion";
+function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+  // jab hme code push karna hota hai server
+  //  tab ise uncomment  karna padega
+  //  useEffect(() => {
+  //   if (window.location.protocol === 'http:') {
+  //     window.location.href = `https://${window.location.hostname}${window.location.pathname}`;
+  //   }
+  // }, []);
+  document.addEventListener("contextmenu", handelRightClick);
+  return (
+    <>
+      <div className="selection:bg-Green selection:text-white">
+        <Navbar></Navbar>
+
+        <FreeQuote />
+        <Routers />
+
+        <Footer />
+      </div>
+      <ScrollToTop />
+    </>
+  );
+}
+
+export default App;
--- src/Routers.js
+++ src/Routers.js
@@ -1,5 +1,5 @@
+import React from 'react'
 import { Routes, Switch, Route } from 'react-router-dom';
-import React, { useEffect } from 'react';
 import Hero from './components/HomePage/Hero';
 import WebDevelopment from './components/InnerPages/Services/Web/WebDevelopment';
 import MobileDevelopment from './components/InnerPages/Services/Mobile/MobileDevelopment';
@@ -18,7 +18,7 @@ import TrainingAndDevelopment from './components/InnerPages/Company/TrainingAndD
 import LifeCerbosys from './components/InnerPages/Company/LifeCerbosys/LifeCerbosys';
 import FreeQuote from './components/Common/Component/FreeQuote';
 import Testimonial from './components/InnerPages/Company/Testimonial/Testimonial';
-import { ScrollToTop } from '../src/components/Common/Component/ScrollToTop';
+import { ScrollToTop } from './components/Common/Component/ScrollToTop';
 import Agriculture from './components/InnerPages/Industries/Agriculture/Agriculture';
 import Banking from './components/InnerPages/Industries/Banking/Banking';
 import Ecommerce from './components/InnerPages/Industries/Ecommerce/Ecommerce';
@@ -30,7 +30,7 @@ import RealEstate from './components/InnerPages/Industries/RealEstate/RealEstate
 import Manufacturing from './components/InnerPages/Industries/Manufacuring/Manufacturing';
 import DotNet from './components/InnerPages/Services/Staff Augmentation/StaffAug InnerPages/.Net/DotNet';
 import Android from './components/InnerPages/Services/Staff Augmentation/StaffAug InnerPages/Android/Android';
-import { handelRightClick } from '../src/components/Common/utils/Desbalrightclick';
+import { handelRightClick } from './components/Common/utils/Desbalrightclick';
 import CaseStudy from './components/CaseStudy/CaseStudy';
 import Angular from './components/InnerPages/Services/Staff Augmentation/StaffAug InnerPages/Angular/Angular';
 import CodeIgniter from './components/InnerPages/Services/Staff Augmentation/StaffAug InnerPages/CodeIgniter/CodeIgniter';
@@ -58,32 +58,10 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Portfolio from './components/protfolio/Portfolio';
 import NotFound from './components/Common/Component/NotFound';
-import Servicethankyou from './components/Common/Component/Thankyou/Servicethankyou';
-import { motion, useScroll, useSpring } from 'framer-motion';
-function App() {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
-  // jab hme code push karna hota hai server
-  //  tab ise uncomment  karna padega 
-  //  useEffect(() => {
-  //   if (window.location.protocol === 'http:') {
-  //     window.location.href = `https://${window.location.hostname}${window.location.pathname}`;
-  //   }
-  // }, []);
-  document.addEventListener('contextmenu', handelRightClick);
+const Routers = () => {
   return (
-    <>
-      <div className="selection:bg-Green selection:text-white">
-        
-        <Navbar></Navbar>
-        
-        <FreeQuote />
-        <Search />
-        <Routes>
+    <div>
+    <Routes>
           <Route exact path="/" element={<Hero />} />
           {/* Company Navbar start */}
           <Route exact path="/overview" element={<Overview />} />
@@ -179,12 +157,9 @@ function App() {
           <Route exact path="/solution/E-Commerse" element={<ECommerse />} />
           <Route exact path="*" element={<NotFound />} />
         </Routes>
-
-        <Footer />
-      </div>
-      <ScrollToTop />
-    </>
-  );
+    
+    </div>
+  )
 }
 
-export default App;
+export default Routers;
