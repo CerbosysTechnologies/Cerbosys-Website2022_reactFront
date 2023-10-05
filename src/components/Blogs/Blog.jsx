d86fa58481b0b726534dfda7705c5c67267ab6cb
CerbosysTechnologies <93573082+CerbosysTechnologies@users.noreply.github.com>
2023-10-05 17:19:51 +0530

    chnages

--- src/components/Blogs/Blog.jsx
+++ src/components/Blogs/Blog.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import Blogsbody from './Blogsbody';
+// import React from 'react';
+// import { Helmet } from 'react-helmet';
+// import Blogsbody from './Blogsbody';
 
-function Blog() {
-  return (
-    <>
-      <Helmet>
-        <title>Blog | Cerbosys</title>
-        <meta
-          name="keywords"
-          content="blog, cerbosys, development experience, customer engagement blogs, web development technologies, web development trends"
-        />
-        {/* <link rel="canonical" href="https://cerbosys.com/blog" /> */}
-        <meta name="description" content="Blog Content" />
-      </Helmet>
-      <div className="selection:bg-Green selection:text-white mx-auto w-full md:h-[38rem]  h-96 py-12 px-4 bg-Blogbg bg-cover md:bg-top bg-center">
-        {/* Content Section */}
-        <div className="flex flex-col justify-center text-center items-center md:h-[35rem] h-96 mx-auto">
-          <h1 className="text-white uppercase font-heading py-2 md:text-3xl text-xl mb-3">
-            Blogs
-          </h1>
-          <p className=" text-center text-white font-subheading   md:text-lg text-xs md:w-[980px] max-w-fit">
-            Explore our development experiences and customer engagement blogs
-            for IT companies. That focuses on Web Development Technologies &
-            Trends.
-          </p>
-        </div>
-        {/* Content Section Ends*/}
-      </div>
-      <div className="selection:bg-Green selection:text-white">
-        <Blogsbody />
-      </div>
-    </>
-  );
-}
+// function Blog() {
+//   return (
+//     <>
+//       <Helmet>
+//         <title>Blog | Cerbosys</title>
+//         <meta
+//           name="keywords"
+//           content="blog, cerbosys, development experience, customer engagement blogs, web development technologies, web development trends"
+//         />
+//         {/* <link rel="canonical" href="https://cerbosys.com/blog" /> */}
+//         <meta name="description" content="Blog Content" />
+//       </Helmet>
+//       <div className="selection:bg-Green selection:text-white mx-auto w-full md:h-[38rem]  h-96 py-12 px-4 bg-Blogbg bg-cover md:bg-top bg-center">
+//         {/* Content Section */}
+//         <div className="flex flex-col justify-center text-center items-center md:h-[35rem] h-96 mx-auto">
+//           <h1 className="text-white uppercase font-heading py-2 md:text-3xl text-xl mb-3">
+//             Blogs
+//           </h1>
+//           <p className=" text-center text-white font-subheading   md:text-lg text-xs md:w-[980px] max-w-fit">
+//             Explore our development experiences and customer engagement blogs
+//             for IT companies. That focuses on Web Development Technologies &
+//             Trends.
+//           </p>
+//         </div>
+       
+//       </div>
+//       <div className="selection:bg-Green selection:text-white">
+//         <Blogsbody />
+//       </div>
+//     </>
+//   );
+// }
 
-export default Blog;
+// export default Blog;
--- src/components/HomePage/DubaiBanner.jsx
+++ src/components/HomePage/DubaiBanner.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import Slide from 'react-reveal/Slide';
+import React from "react";
+import Slide from "react-reveal/Slide";
+import {Link} from "react-router-dom";
 const DubaiBanner = () => {
   return (
-  <div >
-     <Slide top duration={4000}>
-    <div className='mx-auto w-full md:h-[20rem] lg:block hidden h-40  px-4 bg-banner bg-cover bg-no-repeat  bg-center'>
-
-    </div></Slide>
-    <Slide top duration={4000}>
-    <div className='mx-auto w-full md:h-[25rem] lg:hidden block  h-40 py-5 px-4 bg-banner bg-contain bg-no-repeat  bg-center'>
-
+    <div>
+      <Link to="http://gitex.cerbosys.com" target="_blank">
+        <Slide top duration={4000}>
+          <div className="mx-auto w-full md:h-[20rem] lg:block hidden h-40  px-4 bg-banner bg-cover bg-no-repeat  bg-center"></div>
+        </Slide>
+        <Slide top duration={4000}>
+          <div className="mx-auto w-full md:h-[25rem] lg:hidden block  h-40 py-5 px-4 bg-banner bg-contain bg-no-repeat  bg-center"></div>
+        </Slide>
+      </Link>
     </div>
-</Slide>
+  );
+};
 
-  </div>
-  )
-}
-
-export default DubaiBanner
+export default DubaiBanner;
