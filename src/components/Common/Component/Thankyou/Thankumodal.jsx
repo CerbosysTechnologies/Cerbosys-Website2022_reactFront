e51b875138cb720327e3e4285cf37e4e51b21789
CerbosysTechnologies <93573082+CerbosysTechnologies@users.noreply.github.com>
2023-11-20 15:28:21 +0530

    changes

--- src/components/Common/Component/Thankyou/Thankumodal.jsx
+++ src/components/Common/Component/Thankyou/Thankumodal.jsx
@@ -2,42 +2,42 @@ import React from 'react';
 
 const Thankumodal = () => {
   return (
-    <div className="selection:bg-Green selection:text-white">
+    <div classNameName="selection:bg-Green selection:text-white">
       <div
-        className="fixed z-10 overflow-y-auto top-0 w-full left-0 hidden"
+      className="fixed z-10 overflow-y-auto top-0 w-full left-0 hidden"
         id="Thankyouemail"
       >
-        <div className="flex items-center justify-center max-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-          <div className="fixed inset-0 transition-opacity">
-            <div className="absolute inset-0 bg-gray-900 opacity-75" />
+        <div classNameName="flex items-center justify-center max-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div classNameName="fixed inset-0 transition-opacity">
+            <div classNameName="absolute inset-0 bg-gray-900 opacity-75" />
           </div>
-          <span className="hidden sm:inline-block sm:align-middle sm:h-screen">
+          <span classNameName="hidden sm:inline-block sm:align-middle sm:h-screen">
             &#8203;
           </span>
 
           {/* To open contact form */}
           <div
-            className="inline-block 
+          className="inline-block 
              justify-center
              justify-items-center
         
       
             rounded-lg text-left shadow-xl transform transition-all  "
           >
-            <div className="flex items-center justify-center h-screen">
+            <div classNameName="flex items-center justify-center h-screen">
               <div
-                className="p-1 rounded shadow-lg
+              className="p-1 rounded shadow-lg
              
                "
               >
        
                 <div
-                  className="flex flex-col items-center p-4 space-y-2 bg-white rounded-lg  "
+                className="flex flex-col items-center p-4 space-y-2 bg-white rounded-lg  "
                  
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="text-green-600 w-28 h-28"
+                  className="text-green-600 w-28 h-28"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -51,10 +51,10 @@ const Thankumodal = () => {
                   </svg>
                   {/*  text-green-600 */}
                  
-                  <a class="inline-flex items-center px-4 py-2 text-white bg-indigo-600 border border-indigo-600 rounded rounded-full hover:bg-indigo-700 focus:outline-none focus:ring">
+                  <a className="inline-flex items-center px-4 py-2 text-white bg-indigo-600 border border-indigo-600 rounded rounded-full hover:bg-indigo-700 focus:outline-none focus:ring">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      class="w-3 h-3 mr-2"
+                      className="w-3 h-3 mr-2"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
@@ -66,7 +66,7 @@ const Thankumodal = () => {
                         d="M7 16l-4-4m0 0l4-4m-4 4h18"
                       />
                     </svg>
-                    <span className="md:text-lg font-subheading text-sm">
+                    <span classNameName="md:text-lg font-subheading text-sm">
                       <a href="/">Home</a>
                     </span>
                   </a>
