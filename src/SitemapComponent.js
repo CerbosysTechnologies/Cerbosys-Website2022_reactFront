27d08d9ef59fcad4e6b2a3ac1b61041706c70cad
CerbosysTechnologies <cerbosys@gmail.com>
2023-12-15 13:02:40 +0530

    Sitemap Component Changes

--- src/SitemapComponent.js
+++ src/SitemapComponent.js
@@ -1,22 +1,36 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism'; // Choose a style
+import xmlFormatter from 'xml-formatter';
 
 function Sitemap() {
   const [sitemapData, setSitemapData] = useState(null);
 
   useEffect(() => {
-    axios.get('https://api.cerbosys.com/cerbosys/sitemap.xml',{
-      responseType: 'text',
+  axios.get('https://api.cerbosys.com/cerbosys/sitemap.xml',{
+      headers: { Accept: 'application/xml' },
     }).then(response => {
-         const data = response.data;
+        //const data = response.data;
         // console.log('Sitemap => ', data);
         // setSitemapData(data);
 
         // Replace newline characters with HTML line breaks
-      const formattedXml = response.data.replace(/\n/g, '<br>');
-      setSitemapData(data);
-       
-       
+      //const formattedXml = response.data.replace(/\n/g, '<br>');
+      //setSitemapData(data);
+      //const xml = response.text();
+      //setSitemapData(data);
+       // Ensure that the response is XML and not treated as HTML
+       const contentType = response.headers['content-type'];
+       if (contentType && contentType.includes('application/xml')) {
+        const formattedXml = xmlFormatter(response.data, {
+          indentation: '  ', // Set the desired indentation
+          collapseContent: true, // Optional: collapse empty elements
+        });
+         setSitemapData(formattedXml);
+       } else {
+         console.error('Unexpected content type:', contentType);
+       }
        
       
       })
@@ -26,14 +40,42 @@ function Sitemap() {
   }, []);
   return (
     <div className='py-20'>
+      
+      <SyntaxHighlighter language="xml" style={a11yDark}>
+        {sitemapData}
+      </SyntaxHighlighter>
+
     {/* Use <textarea> to render XML content */}
-    <textarea
+    {/* <textarea
       value={sitemapData}
       readOnly
       style={{ width: '100%', height: '400px' }}
-    />
+    /> */}
   </div>
   );
 }
+
+
+const escapeHtml = (unsafe) => {
+  return unsafe.replace(/[&<"']/g, (match) => {
+    switch (match) {
+      case '&':
+        return '&amp;';
+      case '<':
+        return '&lt;';
+      case '"':
+        return '&quot;';
+      case "'":
+        return '&#039;';
+      default:
+        return match;
+    }
+  });
+};
+
+// ...
+
+
+
 
 export default Sitemap;
